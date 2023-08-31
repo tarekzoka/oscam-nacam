be21ba9fcb4796a5c977aacd1b5e7dfc78e1b19f
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2023-09-01 02:38:37 +0300

    Update oscam-os.sh

--- oscam-os.sh
+++ oscam-os.sh
@@ -5,7 +5,7 @@
 ###########################################
 #!/bin/sh
 #
-wget -O /tmp/enigma2-plugin-softcams-oscam_11718-emu-r798_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/enigma2-plugin-softcams-oscam_11718-emu-r798_all.deb?raw=true"
+wget -O /tmp/enigma2-plugin-softcams-oscam_11725-emu-r798_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/enigma2-plugin-softcams-oscam_11725-emu-r798_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
