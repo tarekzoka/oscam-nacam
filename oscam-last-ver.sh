e5b15db0af9c25f81b293b8c91e30b73884bee40
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-02-02 03:20:46 +0200

    Add files via upload

---
+++ oscam-last-ver.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+
+wget -O /tmp/oscam-last-ver.tar.gz "https://raw.githubusercontent.com/tarekzoka/oscam-nacam/main/oscam-last-ver.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/oscam-last-ver.tar.gz
+
+echo "         UPLOADED BY TARK_HANFY    "
+
+killall -9 enigma2
+
+sleep 2;
+
+#!/bin/sh
+#
+
