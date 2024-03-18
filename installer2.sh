480fa0ede2643e3219343b0f718b8183bc385049
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-18 17:19:45 +0200

    Add files via upload

--- installer2.sh
+++ installer2.sh
@@ -13,8 +13,8 @@
 
 PACKAGE_DIR='oscam-nacam/main'
 
-MY_IPK="enigma2-plugin-softcams-oscam_11748.emu.r800_all.ipk"
-MY_DEB="enigma2-plugin-softcams-oscam_11748.emu.r800_all.deb"
+MY_IPK="enigma2-plugin-softcams-oscam_11748-emu-r800_all.ipk"
+MY_DEB="enigma2-plugin-softcams-oscam_11748-emu-r800_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
