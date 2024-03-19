8b33a93624a0d998cc6093929f6ca10323b4bdbe
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-19 17:41:17 +0200

    Add files via upload

--- ncam-os.sh
+++ ncam-os.sh
@@ -7,10 +7,17 @@
 #
 wget -O /tmp/enigma2-plugin-softcams-ncam_V14.6-r0_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/enigma2-plugin-softcams-ncam_V14.6-r0_all.deb?raw=true"
 wait
+#!/bin/sh
+#
+dpkg -i --force-overwrite /tmp/enigma2enigma2-plugin-softcams-ncam_V14.6-r0_all.deb
+wait
+apt-get update && dpkg -i --force-overwrite /tmp/*.deb; apt-get install -f -y
+wait
+opkg install wget
+wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
-wait
-dpkg -i --force-overwrite /tmp/*.deb
-wait
 sleep 2;
+exit 0
+
 
 
