4d22c83d432285cce814773628144ffc7f14b423
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-22 02:08:40 +0200

    Update oscam-os.sh

--- oscam-os.sh
+++ oscam-os.sh
@@ -5,7 +5,7 @@
 ###########################################
 #!/bin/sh
 #
-wget -O /tmp/softcams-oscam_11748-emu-r800_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/softcams-oscam_11748-emu-r800_all.deb?raw=true"
+wget -O /tmp/softcams-oscam_11749-emu-r800_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/softcams-oscam_11749-emu-r800_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
