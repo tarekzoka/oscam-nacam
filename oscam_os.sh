fbb52469ccca193fba1ff2c04306044f335e880e
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-04-27 13:54:44 +0300

    Update oscam_os.sh

--- oscam_os.sh
+++ oscam_os.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-wget -O /tmp/oscam_11766-emu-r801_all.deb "https://raw.githubusercontent.com/tarekzoka/oscam-nacam/main/oscam_11766-emu-r801_all.deb"
+wget -O /tmp/oscam_11770-emu-r801_all.deb "https://raw.githubusercontent.com/tarekzoka/oscam-nacam/main/oscam_11770-emu-r801_all.deb"
 
 dpkg -i --force-overwrite /tmp/*.deb
 
