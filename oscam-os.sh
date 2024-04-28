75de417d567ed58c7dcb844adef3f6f628654f81
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-04-28 23:30:01 +0300

    Update oscam-os.sh

--- oscam-os.sh
+++ oscam-os.sh
@@ -5,7 +5,7 @@
 ###########################################
 #!/bin/sh
 #
-wget -O /tmp/oscam_11770-emu-r801_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/oscam_11770-emu-r801_all.deb?raw=true"
+wget -O /tmp/oscam_11774-emu-r801_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/oscam_11774-emu-r801_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
