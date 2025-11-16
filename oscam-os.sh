5ab8f49e6ba711fcccc23b958be71f4375314b76
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-11-16 16:01:38 +0200

    Update download link for OSCam package

--- oscam-os.sh
+++ oscam-os.sh
@@ -5,7 +5,7 @@
 ###########################################
 #!/bin/sh
 #
-wget -O /tmp/oscam_11869-emu-r802_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/oscam_11869-emu-r802_all.deb?raw=true"
+wget -O /tmp/oscam_11904-emu-r803_all.deb "https://github.com/tarekzoka/oscam-nacam/blob/main/oscam_11904-emu-r803_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
