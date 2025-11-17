3ae988a72f5504ac6d63d0b32fc94ad74f952904
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-11-17 09:27:14 +0200

    Update OS version in oscam_os.sh script

--- oscam_os.sh
+++ oscam_os.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
 
-wget -O /tmp/oscam_11904-emu-r803_all.deb "https://raw.githubusercontent.com/tarekzoka/oscam-nacam/main/oscam_11904-emu-r803_all.deb"
+wget -O /tmp/oscam_11905-emu-r803_all.deb "https://raw.githubusercontent.com/tarekzoka/oscam-nacam/main/oscam_11905-emu-r803_all.deb"
 
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 
-rm -r /tmp/oscam_11904-emu-r803_all.deb
+rm -r /tmp/oscam_11905-emu-r803_all.deb
 
 wait
 
